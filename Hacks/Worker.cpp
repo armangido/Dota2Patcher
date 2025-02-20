ba2ed1119c772a4cf14e1f7fae05d461f310acd2
Alexander Wolf <fake.wolf@2ch.hk>
2025-02-20 15:07:38 +1000

    Weather Changer

--- Hacks/Worker.cpp
+++ Hacks/Worker.cpp
@@ -34,6 +34,7 @@ void Hacks::start_worker() {
                 dota_range_display = vmt.cvar->g_convars["dota_range_display"];
                 vmt.cvar->g_convars["fow_client_nofiltering"]->set<bool>(!ConfigManager::fow_client_nofiltering);
                 vmt.cvar->g_convars["fog_enable"]->set<bool>(!ConfigManager::fog_enabled);
+                vmt.cvar->g_convars["cl_weather"]->set<DOTA_WEATHER>(ConfigManager::cl_weather);
             }
             std::this_thread::sleep_for(std::chrono::seconds(1));
         }
--- Utils/Config.h
+++ Utils/Config.h
@@ -66,9 +66,10 @@ public:
     // HACKS
     static inline bool visible_by_enemy = false;
     static inline bool illusions_detection = false;
+    static inline DOTA_WEATHER cl_weather = DOTA_WEATHER::WEATHER_DEFAULT;
 
     static inline bool hacks_enabled() {
-        return visible_by_enemy || illusions_detection;
+        return visible_by_enemy || illusions_detection || cl_weather != DOTA_WEATHER::WEATHER_DEFAULT;
     }
 
     static inline std::vector<ConfigEntry> config_entries = {
@@ -81,6 +82,7 @@ public:
     { "allow_rc_update", &allow_rc_update },
     { "visible_by_enemy",&visible_by_enemy },
     { "illusions_detection", &illusions_detection },
+    { "cl_weather", &cl_weather },
     };
 
     static void ask_for_settings() {
@@ -91,9 +93,26 @@ public:
         fow_client_nofiltering = ask_for_bool("[~] Remove FoG anti-aliasing? [y/n or 1/0]: ");
         set_rendering_enabled = ask_for_bool("[~] Show hidden particles? [y/n or 1/0]: ");
         allow_rc_update = ask_for_bool("[~] Check for BETA update? [y/n or 1/0]: ");
-        printf("[HACKS] Dota2Patcher will not close!\n");
+        cout << "[HACKS] Dota2Patcher will not close!\n";
         visible_by_enemy = ask_for_bool("[~] Visible By Enemy [y/n or 1/0]: ");
         illusions_detection = ask_for_bool("[~] Illusions Detection [y/n or 1/0]: ");
+
+        // Weather
+        cout << 
+            "[WEATHER]\n"
+            "Default: 1\n"
+            "Snow: 2\n"
+            "Rain: 3\n"
+            "Moonbeam: 4\n"
+            "Pestilence: 5\n"
+            "Harvest: 6\n"
+            "Sirocco: 7\n"
+            "Spring: 8\n"
+            "Ash: 9\n"
+            "Aurora: 10\n";
+
+        std::cin.ignore(10, '\n');
+        cl_weather = (DOTA_WEATHER)(ask_for_int("[~] Enter Weather number: ", 1, 10) - 1);
 
         write_settings();
     }
@@ -111,7 +130,7 @@ public:
             << "[HACKS]\n"
             << "[~] Visible By Enemy: " << std::boolalpha << visible_by_enemy << "\n"
             << "[~] Illusions Detection: " << std::boolalpha << illusions_detection << "\n"
-            ;
+            << "[~] Weather: " << int_to_weather(cl_weather) << "\n";
     }
 
     static bool read_settings() {
@@ -125,6 +144,44 @@ public:
     static void write_settings() {
         for (const auto& entry : config_entries) {
             RegistryConfig::set(entry.name, *reinterpret_cast<int*>(entry.variable));
+        }
+    }
+
+    static string int_to_weather(DOTA_WEATHER weather) {
+        switch (weather) {
+        case DOTA_WEATHER::WEATHER_DEFAULT: {
+            return "Default";
+        }
+        case DOTA_WEATHER::WEATHER_SNOW: {
+            return "Snow";
+        }
+        case DOTA_WEATHER::WEATHER_RAIN: {
+            return "Rain";
+        }
+        case DOTA_WEATHER::WEATHER_MOONBEAM: {
+            return "Moonbeam";
+        }
+        case DOTA_WEATHER::WEATHER_PESTILENCE: {
+            return "Pestilence";
+        }
+        case DOTA_WEATHER::WEATHER_HARVEST: {
+            return "Harvest";
+        }
+        case DOTA_WEATHER::WEATHER_SIROCCO: {
+            return "Sirocco";
+        }
+        case DOTA_WEATHER::WEATHER_SPRING: {
+            return "Spring";
+        }
+        case DOTA_WEATHER::WEATHER_ASH: {
+            return "Ash";
+        }
+        case DOTA_WEATHER::WEATHER_AURORA: {
+            return "Aurora";
+        }
+        default: {
+            return "Default";
+        }
         }
     }
 
