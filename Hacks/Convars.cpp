61662a44cdd6312b4cbd7de2c7a9068ac736405f
Alexander Wolf <fake.wolf@2ch.hk>
2025-05-05 13:06:49 +1000

    dota_new_player removed

--- Hacks/Convars.cpp
+++ Hacks/Convars.cpp
@@ -6,7 +6,6 @@ void Hacks::find_and_set_convars() {
 	GameData::dota_range_display = vmt.cvar->g_convars["dota_range_display"];
 
 	vmt.cvar->g_convars["sv_cheats"]->set<bool>(true);
-	vmt.cvar->g_convars["dota_new_player"]->set<bool>(true);
 	vmt.cvar->g_convars["fow_client_nofiltering"]->set<bool>(ConfigManager::config_entries["fow_client_nofiltering"]);
 	vmt.cvar->g_convars["fog_enable"]->set<bool>(!ConfigManager::config_entries["fog_enabled"]);
 	vmt.cvar->g_convars["cl_weather"]->set<DOTA_WEATHER>((DOTA_WEATHER)ConfigManager::config_entries["cl_weather"]);
