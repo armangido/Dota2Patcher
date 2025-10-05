b7be89c19bfae8ffd8a9106bf33c25a907d225d2
Arito <42208098+armangido@users.noreply.github.com>
2025-10-06 03:19:07 +0400

    Update CDOTAGamerules.h

--- SourceSDK/CDOTAGamerules.h
+++ SourceSDK/CDOTAGamerules.h
@@ -10,12 +10,12 @@ public:
 
     DOTA_GAME_STATE game_state() const {
         const auto game_state = Memory::read_memory<int>(this + 0x78);
+        std::cout << game_state;
         return game_state.has_value() ? static_cast<DOTA_GAME_STATE>(game_state.value()) : DOTA_GAME_STATE::INVALID;
     }
 
     bool in_game() const {
         const auto game_state = this->game_state();
-        std::cout << game_state;
         return game_state == DOTA_GAME_STATE::HERO_SELECTION || game_state == DOTA_GAME_STATE::STRATEGY_TIME || game_state == DOTA_GAME_STATE::PRE_GAME || game_state == DOTA_GAME_STATE::GAME_IN_PROGRESS || game_state == DOTA_GAME_STATE::CUSTOM_GAME_SETUP;
     }
 };
